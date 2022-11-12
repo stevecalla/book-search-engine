01b0ebf2a155ba1138a8a22fd8204ede04cd1016
Steve Calla <callasteven@gmail.com>
2022-11-12 02:58:34 -0700

    Create function to eliminate duplicate books returned by google api

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -41,15 +41,42 @@ const SearchBooks = () => {
 
       const { items } = await response.json();
 
-      const bookData = items.map((book) => ({
+      // results above can return duplicate book id; process below removes duplicates
+      // when duplicate entries exist reeact returns an error message in the console b/c it can't uniquely identify each item
+      const allBookIds = items.map(book => book.id);
+      const allBooks = items.map(book => book.volumeInfo);
+
+      let duplicateBookIds = [];
+      let uniqueBookIds = [];
+      let uniqueBooks = [];
+
+      for (let i = 0; i < allBookIds.length; i++) {
+        if (!uniqueBookIds.includes(allBookIds[i])) {
+          uniqueBookIds.push(allBookIds[i]);
+          uniqueBooks.push(allBooks[i]);
+        } else {
+          duplicateBookIds.push({id: allBookIds[i], index: i});
+        }
+      };
+
+      //add book id to the unique book objects
+      for (let i = 0; i < uniqueBooks.length; i++) {
+        uniqueBooks[i].id = uniqueBookIds[i];
+      }
+
+      console.log({duplicateBookIds});
+      console.log({uniqueBookIds});
+      console.log({uniqueBooks});
+
+      const bookData = uniqueBooks.map((book) => ({
         bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description || "No description available.",
-        image: book.volumeInfo.imageLinks?.thumbnail || "https://placehold.jp/16/0000FF/ffffff/300x500.png?text=No%20Image%20Available",
-        publishedDate: book.volumeInfo.publishedDate || "No publish date",
-        previewLink: book.volumeInfo.previewLink || "No preview link",
-        infoLink: book.volumeInfo.infoLink || "No info link",
+        authors: book.authors || ['No author to display'],
+        title: book.title,
+        description: book.description || "No description available.",
+        image: book.imageLinks?.thumbnail || "https://placehold.jp/16/0000FF/ffffff/300x500.png?text=No%20Image%20Available",
+        publishedDate: book.publishedDate || "No publish date",
+        previewLink: book.previewLink || "No preview link",
+        infoLink: book.infoLink || "No info link",
       }));
 
       console.log(bookData);
