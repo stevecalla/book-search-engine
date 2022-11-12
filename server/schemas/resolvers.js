daa9857312848f6711d44b8a82b82d1de50b6185
Steve Calla <callasteven@gmail.com>
2022-11-12 11:20:50 -0700

    Add context to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,32 +1,24 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async (parent, args, context) => {
-    // if (context.user) {
-      return User.find().populate('books');
-    // }
-    //   throw new AuthenticationError('You need to be logged in!');
-    },
-    user: async (parent, { email }) => {
-      return User.findOne({ email }).populate('books');
+      if (context.user) {
+        return User.find().populate("books");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
     books: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Book.find(params).sort({ createdAt: -1 });
     },
-    // me: async (parent, args, context) => {
-    //   console.log(context, context.user);
-    //   // if (context.user) {
-    //     // return User.findOne({ _id: "636c6732dd1ce92e610cd132" }).populate('books');
-    //     return User.findOne({ _id: context.user._id }).populate('books');
-    //   // }
-    //   // throw new AuthenticationError('You need to be logged in!');
-    // },
     me: async (parent, { _id }, context) => {
-        return User.findById({ _id }).populate('books');
+      if (context.user) {
+        return User.findById({ _id: context.user._id }).populate("books");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 
@@ -40,24 +32,39 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
 
       return { token, user };
     },
-    addBook: async (parent, { _id, bookId, authors, description, image, title, infoLink, previewLink, publishedDate }, context) => {
-      // if (context.user) {
+    addBook: async (
+      parent,
+      {
+        _id,
+        bookId,
+        authors,
+        description,
+        image,
+        title,
+        infoLink,
+        previewLink,
+        publishedDate,
+      },
+      context
+    ) => {
+      if (context.user) {
         return User.findOneAndUpdate(
           { _id },
-          { $addToSet: {
+          {
+            $addToSet: {
               savedBooks: {
                 bookId: bookId,
                 authors,
@@ -66,29 +73,31 @@ const resolvers = {
                 title,
                 infoLink,
                 previewLink,
-                publishedDate
+                publishedDate,
               },
             },
           },
           { new: true }
         );
-     },
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
     removeBook: async (parent, { _id, bookId }, context) => {
-      // if (context.user) {
-        //todo
+      if (context.user) {
         return User.findOneAndUpdate(
           { _id },
-          { $pull: {
+          {
+            $pull: {
               savedBooks: {
-                bookId: bookId
+                bookId: bookId,
               },
             },
           },
           { new: true }
         );
-     },
-    //   throw new AuthenticationError('You need to be logged in!');
-    // },
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 };
 
