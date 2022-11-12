d35c753f59ff6c36d6c867a81c1fd1930e9880ca
Steve Calla <callasteven@gmail.com>
2022-11-12 06:57:33 -0700

    Move duplicate books function to utils

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,6 +8,7 @@ import { ADD_BOOK } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { removeDuplicateBooks } from '../utils/removeDuplicateBooks';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -41,32 +42,9 @@ const SearchBooks = () => {
 
       const { items } = await response.json();
 
-      // results above can return duplicate book id; process below removes duplicates
+      // results in "items" can return duplicate book id; function below removes duplicates
       // when duplicate entries exist reeact returns an error message in the console b/c it can't uniquely identify each item
-      const allBookIds = items.map(book => book.id);
-      const allBooks = items.map(book => book.volumeInfo);
-
-      let duplicateBookIds = [];
-      let uniqueBookIds = [];
-      let uniqueBooks = [];
-
-      for (let i = 0; i < allBookIds.length; i++) {
-        if (!uniqueBookIds.includes(allBookIds[i])) {
-          uniqueBookIds.push(allBookIds[i]);
-          uniqueBooks.push(allBooks[i]);
-        } else {
-          duplicateBookIds.push({id: allBookIds[i], index: i});
-        }
-      };
-
-      //add book id to the unique book objects
-      for (let i = 0; i < uniqueBooks.length; i++) {
-        uniqueBooks[i].id = uniqueBookIds[i];
-      }
-
-      console.log({duplicateBookIds});
-      console.log({uniqueBookIds});
-      console.log({uniqueBooks});
+      let uniqueBooks = removeDuplicateBooks(items);
 
       const bookData = uniqueBooks.map((book) => ({
         bookId: book.id,
